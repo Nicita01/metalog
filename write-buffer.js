4d118e2fff028d26c55573f9fd053f3ad9c1a2bd
Nicita01 <nicitamac@gmail.com>
2018-06-03 19:06:15 +0300

    Implement write method

--- write-buffer.js
+++ write-buffer.js
@@ -16,3 +16,36 @@ function WriteBuffer(length, timeout, maxLength) {
 }
 
 util.inherits(WriteBuffer, EventEmitter);
+
+WriteBuffer.prototype.write = function(buffer) {
+  const freeSize = this.length - this.position;
+  const messageLength = buffer.length;
+  if (freeSize >= messageLength) {
+    buffer.copy(this.buffer, this.position);
+    clearTimeout(this.writeTimer);
+    this.writeTimer = setTimeout(() => {
+      this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
+    }, this.writeInterval);
+    this.position += messageLength;
+  } else if (messageLength < this.length) {
+    this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
+    this.position = 0;
+    this.write(buffer);
+  } else if (messageLength < this.maxLength) {
+    this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
+    this.position = 0;
+    let cntBytesTransmit = this.length;
+    for (; cntBytesTransmit < messageLength; cntBytesTransmit += this.length) {
+      this.emit('data', null, buffer.slice(
+        cntBytesTransmit - this.length, cntBytesTransmit
+      ));
+    }
+    this.write(buffer.slice(cntBytesTransmit - this.length, messageLength));
+  } else {
+    this.emit('data', new Error('message is to large'));
+  }
+};
+
+WriteBuffer.prototype.drop = function(start = 0, end = this.position) {
+  this.emit('data', null, Buffer.from(this.buffer.slice(start, end)));
+};
