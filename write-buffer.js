ca29948e07c4eded1206abd236c8b04b6ac239e9
Nicita01 <nicitamac@gmail.com>
2018-06-03 22:15:33 +0300

    Refactor

--- write-buffer.js
+++ write-buffer.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const util = require('util');
 const EventEmitter = require('events');
+const common = require('metarhia-common');
 
 const DEFAULT_LENGTH = 16385;
 const MAX_LENGTH_MESSAGE = 10 * DEFAULT_LENGTH;
@@ -11,31 +11,30 @@ function WriteBuffer(length, timeout, maxLength) {
   this.writeInterval = timeout || null;
   this.position = 0;
   this.maxLength = maxLength || MAX_LENGTH_MESSAGE;
-  this.buffer = Buffer.allocUnsafe(this.length);
+  this.buffer = Buffer.allocUnsafe(length);
   this.writeTimer = null;
 }
 
-util.inherits(WriteBuffer, EventEmitter);
+common.inherits(WriteBuffer, EventEmitter);
 
 WriteBuffer.prototype.write = function(buffer) {
   const freeSize = this.length - this.position;
   const messageLength = buffer.length;
   if (freeSize >= messageLength) {
-    buffer.copy(this.buffer, this.position);
+    this.position += buffer.copy(this.buffer, this.position);
     clearTimeout(this.writeTimer);
     this.writeTimer = setTimeout(() => {
       this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
     }, this.writeInterval);
-    this.position += messageLength;
-  } else if (messageLength < this.length) {
+  } else if (messageLength <= this.length) {
     this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
     this.position = 0;
     this.write(buffer);
-  } else if (messageLength < this.maxLength) {
+  } else if (messageLength <= this.maxLength) {
     this.emit('data', null, Buffer.from(this.buffer.slice(0, this.position)));
     this.position = 0;
     let cntBytesTransmit = this.length;
-    for (; cntBytesTransmit < messageLength; cntBytesTransmit += this.length) {
+    for (; cntBytesTransmit <= messageLength; cntBytesTransmit += this.length) {
       this.emit('data', null, buffer.slice(
         cntBytesTransmit - this.length, cntBytesTransmit
       ));
